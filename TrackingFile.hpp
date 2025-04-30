b72686d9a0e2ecb1043f51894b9afb2e388edf84
maksd <maksdemeschenko@gmail.com>
2025-04-30 10:57:09 +0300

    Created data structure to handle required functionality

---
+++ TrackingFile.hpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <list>
+#include <chrono>
+#include <uuid/uuid.h>
+
+struct FileChange
+{
+    std::chrono::system_clock::time_point timestamp; 
+    std::string changeType;                          
+    std::string checksum;                            
+    std::string savedVersionId;                      
+    std::string user;                                
+    std::string additionalInfo;                      
+};
+
+struct FileHistory
+{
+    std::list<FileChange> changes;
+};
+
+struct TrackingFile
+{
+    std::string filePath;      
+    std::string fileId;        
+    FileHistory history;       
+    std::string lastChecksum;  
+    bool isMissing = false;    
+};
+
+
+
+bool rollbackTo(const FileChange& change);
+
+bool rollbackToVersion(const std::string& versionId);
